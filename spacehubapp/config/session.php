2ccf82bccd6e7cf47856ec9e7530ddf131f4f01f
Ngeno Mark <kipchumbamark48@gmail.com>
2024-05-22 21:09:16 +0300

    Update on all laravel files and enabled email  verification

--- spacehubapp/config/session.php
+++ spacehubapp/config/session.php
@@ -32,8 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
-
+    'lifetime' => env('SESSION_LIFETIME', 20), // This value is in minutes. Ensure it's longer than your inactivity timeout.
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
     /*
