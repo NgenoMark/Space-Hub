2e70247f8411b8369aaa171bc2c063ab91cf2182
NgenoMark <kipchumbamark48@gmail.com>
2024-05-25 21:08:33 +0300

    creating a javascript file for tracking time

---
+++ spacehubapp/public/js/inactivity_logout.js
@@ -0,0 +1,31 @@
+let inactivityTime = function () {
+    let time;
+    let maxInactivityTime = 10 * 60 * 1000; // 10 minutes
+    let warningTime = 9 * 60 * 1000; // 9 minutes (1 minute before logout)
+
+    window.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    document.onclick = resetTimer;
+    document.onscroll = resetTimer;
+    document.onwheel = resetTimer;
+    document.onkeydown = resetTimer;
+
+    function logout() {
+        axios.post('/logout').then(response => {
+            window.location.href = '/login';
+        });
+    }
+
+    function showWarning() {
+        alert('You will be logged out in 1 minute due to inactivity.');
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logout, maxInactivityTime);
+        setTimeout(showWarning, warningTime);
+    }
+};
+
+inactivityTime();
