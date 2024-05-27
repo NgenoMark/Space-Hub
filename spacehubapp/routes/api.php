6c127e313dd9a324bb92e7cc34233fefc4a44706
NgenoMark <kipchumbamark48@gmail.com>
2024-05-27 13:01:00 +0300

    added a route for middleware

--- spacehubapp/routes/api.php
+++ spacehubapp/routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\InactiveLogoutController;
 
+// routes/api.php
+
+Route::middleware('auth:sanctum')->post('/user/inactive-logout', [InactiveLogoutController::class, 'logout']);
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
